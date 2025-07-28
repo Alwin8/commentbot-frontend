cfeaca2c7a5a3fc7aed52574e246fb946e794f69
Alwin <lionpswn@gmail.com>
2025-07-28 18:53:04 +0530

    v1.17

--- src/pages/Dashboard/fbConnect.tsx
+++ src/pages/Dashboard/fbConnect.tsx
@@ -6,7 +6,7 @@ interface Props{
     isAuthenticated:boolean
 }
 function FbConnect(props:Props){
-     const selfUrl="https://637lnk48-5173.inc1.devtunnels.ms/dashboard"
+     const selfUrl=import.meta.env.VITE_FRONTEND+"/dashboard"
      const fbRedirectLink="https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=1063855271249467&redirect_uri="+selfUrl+"&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights"
     return (
         <div className="fullviewport d-grid justify-content-center">
